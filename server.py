09a5d03c12782a0508f82d98ecde9482914038c0
eswar-cv <es.reddycv@gmail.com>
2023-08-20 12:51:05 +0530

    added content

--- server.py
+++ server.py
@@ -1,19 +1,21 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return ''
+    return 'Hello world!'
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    return ''
+    content = request.get_json()
+    return {'result': content['first'] + content['second']}
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    return ''
+    content = request.get_json()
+    return {'result': content['first'] - content['second']}
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
